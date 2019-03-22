e06ac755fc02ea9323e47ff82343beec089a208d
Gongo <jvilla1714@gmail.com>
2019-03-22 13:27:06 -0400

    Finished UI

--- Hexagon/Project1/Hexagon.cpp
+++ Hexagon/Project1/Hexagon.cpp
@@ -39,17 +39,82 @@ void Hexagon::add_edge(int i, int j){
 	graph[j][i] = 0;
 }
 void Hexagon::modify_state(int i, int j) {
-	int min = std::min(i, j);
-	int max = std::max(i, j);
 	if (table[i].position_x == table[j].position_x) {
-		for (int k = table[min].position_y + 1; k < table[max].position_y; k++) {
-			UI[table[min].position_x][k] = '-';
+		int min = std::min(table[i].position_y, table[j].position_y);
+		int max = std::max(table[i].position_y, table[j].position_y);
+		for (int k = min + 1; k < max; k++) {
+			UI[table[i].position_x][k] = '*';
 		}
 	}
 	if (table[i].position_y == table[j].position_y) {
-		for (int k = table[min].position_x + 1; k < table[max].position_x; k++) {
-			UI[k][table[min].position_y] = '|';
+		int min = std::min(table[i].position_x, table[j].position_x);
+		int max = std::max(table[i].position_x, table[j].position_x);
+		for (int k = min + 1; k < max; k++) {
+			UI[k][table[i].position_y] = '*';
 		}
+	}
+	int min = std::min(i, j);
+	int max = std::max(i,j);
+	if (min == 0 && max == 5) {
+		UI[1][1] = '*';
+		UI[2][1] = '*';
+		UI[3][1] = '*';
+	}
+	if (min == 4 && max == 5) {
+		UI[5][1] = '*';
+		UI[6][1] = '*';
+		UI[7][1] = '*';
+	}
+	if (min == 1 && max == 2) {
+		UI[1][7] = '*';
+		UI[2][7] = '*';
+		UI[3][7] = '*';
+	}
+	if (min == 2 && max == 3) {
+		UI[5][7] = '*';
+		UI[6][7] = '*';
+		UI[7][7] = '*';
+	}
+	if (min == 3 && max == 5) {
+		UI[4][1] = '*';
+		UI[5][2] = '*';
+		UI[6][3] = '*';
+		UI[7][4] = '*';
+		UI[8][5] = '*';
+	}
+	if (min == 1 && max == 5) {
+		UI[4][1] = '*';
+		UI[3][2] = '*';
+		UI[2][3] = '*';
+		UI[1][4] = '*';
+		UI[0][5] = '*';
+	}
+	if (min == 2 && max == 4) {
+		UI[8][3] = '*';
+		UI[7][4] = '*';
+		UI[6][5] = '*';
+		UI[5][6] = '*';
+		UI[4][7] = '*';
+	}
+	if (min == 1 && max == 4) {
+		UI[8][3] = '*';
+		UI[7][4] = '*';
+		UI[6][4] = '*';
+		UI[5][4] = '*';
+		UI[4][4] = '*';
+		UI[3][4] = '*';
+		UI[2][4] = '*';
+		UI[1][5] = '*';
+	}
+	if (min == 0 && max == 3) {
+		UI[8][5] = '*';
+		UI[7][4] = '*';
+		UI[6][4] = '*';
+		UI[5][4] = '*';
+		UI[4][4] = '*';
+		UI[3][4] = '*';
+		UI[2][4] = '*';
+		UI[1][3] = '*';
 	}
 }
 void Hexagon::show_state() {
